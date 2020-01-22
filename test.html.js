c6e9cf6ba15d16798bc45a10a556630c198fe707
Alexey Vavilin <mnogoborec@mail.ru>
2020-01-22 21:48:54 +0300

    Timings in test.html.js

--- test.html.js
+++ test.html.js
@@ -1,6 +1,15 @@
+let prev = Date.now() / 1000;
+function logTime(message) {
+    const now = Date.now() / 1000;
+    console.log(message, now, String((now - prev).toFixed(3)));
+    prev = now;
+}
+
 const inlineAssetsLoader = import('./inline-assets.js');
+logTime("0");
 
 const createLibrary = async () => {
+    logTime("1");
     const inlineAssets = (await inlineAssetsLoader).default;
     const workerBlob = new Blob(
         [inlineAssets.worker],
@@ -8,6 +17,7 @@ const createLibrary = async () => {
     );
     const workerUrl = URL.createObjectURL(workerBlob);
     const worker = new Worker(workerUrl);
+    logTime("2");
 
     const activeRequests = new Map();
 
@@ -19,11 +29,13 @@ const createLibrary = async () => {
 
     const library = {
         request: (method, params, callback) => {
+            logTime("3");
             const id = nextActiveRequestId;
             nextActiveRequestId += 1;
             activeRequests.set(id, {
                 callback
             });
+            logTime("4");
             worker.postMessage({
                 request: {
                     id,
@@ -31,11 +43,14 @@ const createLibrary = async () => {
                     params
                 }
             });
+            logTime("5");
         },
     };
     let libraryResolver = null;
+    logTime("6");
 
     worker.onmessage = (evt) => {
+        logTime("7");
         const setup = evt.data.setup;
         if (setup) {
             if (libraryResolver) {
@@ -43,6 +58,7 @@ const createLibrary = async () => {
             }
             return;
         }
+        logTime("8");
 
         const response = evt.data.response;
         if (response) {
@@ -50,7 +66,9 @@ const createLibrary = async () => {
             if (!activeRequest) {
                 return;
             }
+            logTime("9");
             activeRequests.delete(response.id);
+            logTime("10");
             if (activeRequest.callback) {
                 let { result } = response;
                 // Remove BOM from result
@@ -58,10 +76,12 @@ const createLibrary = async () => {
                 const { result_json, error_json } = JSON.parse(result);
                 activeRequest.callback(result_json, error_json);
             }
+            logTime("11");
         }
     };
 
     return new Promise((resolve) => {
+        logTime("12");
         libraryResolver = resolve;
         worker.postMessage({
             setup: {
@@ -69,24 +89,33 @@ const createLibrary = async () => {
                 wasmWrapper: inlineAssets.wasmWrapper,
             }
         });
+        logTime("13");
     });
 };
 
 window.onload = () => {
     (async () => {
+        logTime("14");
         const library = await createLibrary();
+        logTime("15");
         const request = (method, params) => {
             return new Promise((resolve, reject) => {
+                logTime("16");
                 library.request(method, params, (result, err) => {
+                    logTime("17");
                     if (err !== '') {
                         reject(err);
                     } else {
                         resolve(JSON.parse(result));
                     }
+                    logTime("18");
                 });
             });
         };
+        logTime("19");
         const version = await request('version', '12');
+        logTime("20");
         document.body.innerText = `TON Client Version: ${version}`;
+        logTime("21");
     })();
 };
